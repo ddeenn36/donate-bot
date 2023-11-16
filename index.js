087756e01e9ae61f070ecd052cccdd6f32c4d536
den <mac@macbook-pro-mac.localdomain>
2023-11-17 00:23:34 +0100

    .

--- index.js
+++ index.js
@@ -4,34 +4,30 @@ const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 const webAppUrl = 'https://astonishing-cat-d65e6c.netlify.app/';
 
-async function start() {
-	bot.on('message', async (msg) => {
-		const chatId = msg.chat.id;
-		const text = msg.text;
-	
-		if (text === '/start') {
-			await bot.sendMessage(chatId, 'Hello!', {
-				reply_markup: {
-					keyboard: [
-						[{text: 'Open donate app', web_app: {url: webAppUrl}}]
-					]
-				}
-			});
-		}
-	});
+bot.on('message', async (msg) => {
+	const chatId = msg.chat.id;
+	const text = msg.text;
 
-	bot.onText(/\/start \w/, async (msg) => {
-		const text = msg.text;
-		const arg = text.split(' ')[1];
-
-		await bot.sendMessage(msg.chat.id, 'Make a donation!', {
+	if (text === '/start') {
+		await bot.sendMessage(chatId, 'Hello!', {
 			reply_markup: {
-				inline_keyboard: [
-					[{text: 'Donate', web_app: {url: webAppUrl+arg}}]
+				keyboard: [
+					[{text: 'Open donate app', web_app: {url: webAppUrl}}]
 				]
 			}
 		});
-	});
-}
+	}
+});
 
-start();
+bot.onText(/\/start .+/, async (msg) => {
+	const text = msg.text;
+	const arg = text.split(' ')[1];
+
+	await bot.sendMessage(msg.chat.id, 'Make a donation!', {
+		reply_markup: {
+			inline_keyboard: [
+				[{text: 'Donate', web_app: {url: webAppUrl+arg}}]
+			]
+		}
+	});
+});
