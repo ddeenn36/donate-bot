3b64f241916dd483efcf3c6b7005d1a8388bd7eb
den <mac@macbook-pro-mac.localdomain>
2023-11-17 22:42:16 +0100

    .

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const TelegramBot = require("node-telegram-bot-api");
 
 const token = process.env.BOT_TOKEN;
-const bot = new TelegramBot(token, { polling: true });
-const webAppUrl = 'https://astonishing-cat-d65e6c.netlify.app/';
+const options = {
+  polling: true
+};
+const bot = new TelegramBot(token, options);
+// const webAppUrl = 'https://astonishing-cat-d65e6c.netlify.app/';
 
-bot.onText(/\/start/, async (msg) => {
-	await bot.sendMessage(chatId, 'Hello!', {
-		reply_markup: {
-			reply_markup: [
-				[{text: 'Open donate app', web_app: {url: webAppUrl}}]
-			]
-		}
-	});
+bot.onText(/\start/, (msg) => {
+	bot.sendMessage(msg.chat.id, 'Hi');
 });
+
+// bot.onText(/\/start/, async (msg) => {
+// 	await bot.sendMessage(chatId, 'Hello!', {
+// 		reply_markup: {
+// 			reply_markup: [
+// 				[{text: 'Open donate app', web_app: {url: webAppUrl}}]
+// 			]
+// 		}
+// 	});
+// });
 
 // bot.onText(/\/start .+/, async (msg) => {
 // 	const text = msg.text;
